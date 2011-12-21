04421c9cca96bbe230921b8a65e6c097916b5369
arnab <arnab.deka@gmail.com>
2011-12-20 23:13:05 -0800

    Default font size back to 14pt

--- arnab/look-n-feel.el
+++ arnab/look-n-feel.el
@@ -2,7 +2,7 @@
 (setq initial-frame-alist '((width . 200) (height . 60)))
 
 ; Font: Inconsolata 12pt
-(set-default-font "-apple-Inconsolata-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(set-default-font "-apple-Inconsolata-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
 
 ; http://www.emacswiki.org/emacs/ColorTheme#toc9
 (require 'color-theme)
