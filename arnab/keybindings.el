ca7edd1564e3cc341ff0de12b74e6a5f244a95a3
Arnab Deka <arnab.deka@gmail.com>
2012-09-15 18:30:00 +0530

    keybinds to get to settings

--- arnab/keybindings.el
+++ arnab/keybindings.el
@@ -1,4 +1,5 @@
 ;; something sets C-a to mark-whole-buffer. Set it back.
 (global-set-key (kbd "C-a") 'move-to-left-margin)
 
-(global-set-key (kbd "s-,") (find-file "~/code/os/emacs-starter-kit/arnab.el"))
+;; settings
+(global-set-key (kbd "s-,") (lambda()(interactive)(find-file "~/code/os/emacs-starter-kit/")))
