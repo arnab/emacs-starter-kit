bca70220a1269059d6cc8d7a9acd6ce6f48298b0
Arnab Deka <arnab.deka@gmail.com>
2012-09-15 18:07:21 +0530

    stuff added bu sublime.el

--- init.el
+++ init.el
@@ -18,3 +18,40 @@
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(backup-inhibited t t)
+ '(cursor-type (quote bar) t)
+ '(custom-safe-themes (quote ("71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" default)))
+ '(echo-keystrokes 0.01)
+ '(fill-column 78)
+ '(frame-title-format (quote ("%f - " user-real-login-name "@" system-name)) t)
+ '(ido-auto-merge-work-directories-length nil)
+ '(ido-create-new-buffer (quote always))
+ '(ido-enable-flex-matching t)
+ '(ido-enable-prefix nil)
+ '(ido-everywhere t)
+ '(ido-ignore-extensions t)
+ '(ido-max-prospects 8)
+ '(ido-use-filename-at-point (quote guess))
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-screen t)
+ '(linum-format "  %d  ")
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
+ '(puppet-indent-level tab-width)
+ '(recentf-max-saved-items 75)
+ '(require-final-newline t)
+ '(ruby-indent-level tab-width)
+ '(show-paren-delay 0)
+ '(tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
