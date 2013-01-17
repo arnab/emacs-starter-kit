946e6d48220cb25c39f4426af31923486a6948cb
Arnab Deka <arnab.deka@gmail.com>
2013-01-17 11:45:29 +0530

    helm-ls-git setup so it appears in helm-mini buffer

--- arnab/helm-setup.el
+++ arnab/helm-setup.el
@@ -1,3 +1,18 @@
 (add-to-list 'load-path "~/.emacs.d/elpa/helm-20130114.2254")
 (require 'helm-config)
 (helm-mode 1)
+
+(add-to-list 'load-path "~/.emacs.d/elpa/helm-ls-git-20130114.1739")
+(require 'helm-ls-git)
+
+;;;###autoload
+(defun helm-mini ()
+  "My additions (all git file) to Preconfigured `helm' lightweight version \(buffer -> recentf\)."
+  (interactive)
+  (require 'helm-files)
+  (helm-other-buffer '(helm-c-source-buffers-list
+                       helm-c-source-recentf
+                       helm-c-source-ls-git-status
+                       helm-c-source-ls-git
+                       helm-c-source-buffer-not-found)
+                     "*helm mini*"))
