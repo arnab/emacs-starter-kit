4df2d954d97b850d20d6b43660b16a682fafb6dc
Arnab Deka <arnab.deka@gmail.com>
2013-01-31 12:34:13 +0530

    Fonts adjustment for emacs inside a terminal

--- arnab/look-n-feel.el
+++ arnab/look-n-feel.el
@@ -1,15 +1,18 @@
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 ;; (load-theme 'solarized-light t)
 
+(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
+(set-frame-parameter nil 'font "DejaVu Sans Mono-10")
+
 (defun fontify-frame (frame)
   (interactive)
   (if window-system
       (progn
         (if (> (x-display-pixel-width) 2000)
             (set-frame-parameter frame 'font "Inconsolata 19") ;; Cinema Display
-         (set-frame-parameter frame 'font "Inconsolata 15.5")))))
+          (set-frame-parameter frame 'font "Inconsolata 15.5")))))
 
-  ;; Fontify current frame
+;; Fontify current frame
 (fontify-frame nil)
 ;; Fontify any future frames
 (push 'fontify-frame after-make-frame-functions)
