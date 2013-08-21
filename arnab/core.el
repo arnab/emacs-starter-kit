c1566406c7b10d3adf65816dfcaa3efed077866b
Arnab Deka <arnab.deka@gmail.com>
2013-08-21 17:48:40 +0530

    set up PATH in Emacs UI too

--- arnab/core.el
+++ arnab/core.el
@@ -5,3 +5,12 @@
 
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
+
+;; From http://clojure-doc.org/articles/tutorials/emacs.html
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
