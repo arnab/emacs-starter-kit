0b30b9326eaa58fe51f50b6d8e3924ff2637a276
Arnab Deka <arnab.deka@gmail.com>
2013-08-22 12:23:58 +0530

    set fonts the Emacs24 way.

--- arnab/look-n-feel.el
+++ arnab/look-n-feel.el
@@ -1,22 +1,21 @@
-(load-theme 'zenburn t)
+(load-theme 'molokai t)
+(blink-cursor-mode 1)
 
-(add-to-list 'default-frame-alist '(font . "DejaVu Sans Mono-10"))
-(set-frame-parameter nil 'font "DejaVu Sans Mono-10")
-
-(add-to-list 'default-frame-alist '(font . "Source Code Pro-15"))
-(set-frame-parameter nil 'font "Source Code Pro-15")
+(require 'dash)
+(defun font-candidate (&rest fonts)
+  "Return the first available font."
+  (--first (find-font (font-spec :name it)) fonts))
 
 (defun fontify-frame (frame)
   (interactive)
   (if window-system
       (progn
-        (if (> (x-display-pixel-width) 2000)
-            (set-frame-parameter frame 'font "Inconsolata 19") ;; Cinema Display
-          (set-frame-parameter frame 'font "Inconsolata 15.5")))))
-
+        (set-face-attribute 'default nil :font
+                    (font-candidate '"Source Code Pro-18"
+                                    "DejaVu Sans Mono-18"
+                                    "Droid Sans Mono-18"
+                                    "Consolas-18" )))))
 ;; Fontify current frame
 (fontify-frame nil)
 ;; Fontify any future frames
 (push 'fontify-frame after-make-frame-functions)
-
-(blink-cursor-mode 1)
