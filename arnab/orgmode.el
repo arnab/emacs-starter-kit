97a47b3d80c08cdefbe1c15c6b65c854616e62a3
Arnab Deka <arnab.deka@gmail.com>
2013-08-30 15:50:24 +0530

    org dir setup.

--- arnab/orgmode.el
+++ arnab/orgmode.el
@@ -1,6 +1,8 @@
 (org-mode)
 (org-indent-mode)
-(setq org-default-notes-file (concat org-directory "~/Dropbox/notes.org"))
+(setq org-default-notes-file (concat org-directory "~/Dropbox/org-mode/notes.org"))
+(setq org-directory "~/Dropbox/org-mode")
+
 (define-key global-map "\C-cc" 'org-capture)
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
