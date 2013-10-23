975d8ec0af094680c51bdb21cfa138a4b19aaaf0
Arnab Deka <arnab.deka@gmail.com>
2013-10-23 14:46:44 +0530

    super useful buffer-move.

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings
                                   starter-kit-eshell starter-kit-js
                                   starter-kit-ruby ruby-end ruby-block
-                                  ag unfill csv-mode
+                                  ag unfill csv-mode buffer-move
                                   rbenv rspec-mode ruby-test-mode
                                   magit textmate helm helm-ls-git
                                   clojure-mode clojure-test-mode nrepl
