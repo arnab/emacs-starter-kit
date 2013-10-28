4b95ac431dba89933f904d8d920ee42996a88709
Arnab Deka <arnab.deka@gmail.com>
2013-10-28 22:27:44 +0530

    set up powerline.

---
+++ arnab/powerline.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/elpa/emacs-powerline")
+(require 'powerline)
+
+;; (setq powerline-color1 "grey20")
+;; (setq powerline-color2 "grey40")
+
+(set-face-attribute 'mode-line nil
+                    :background "Blue")
