f3f028db252d694eda884fbc76b845883a050adc
Arnab Deka <arnab.deka@gmail.com>
2013-10-30 12:45:48 +0530

    use robe.

--- arnab/ruby.el
+++ arnab/ruby.el
@@ -1,6 +1,25 @@
+;;; rbenv
+(setq rbenv-installation-dir "/usr/local/opt/rbenv/")
 (require 'rbenv)
-;; (global-rbenv-mode)
+(global-rbenv-mode)
 (custom-set-variables '(ruby-test-ruby-executables (list rbenv-ruby-shim)))
+
+;;; Robe
+(require 'robe)
+(add-hook 'enh-ruby-mode-hook 'robe-mode)
+
+;;; enhanced ruby mode
+(setq enh-ruby-program "/usr/local/opt/rbenv/versions/2.0.0-p195/bin/ruby")
+(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rabl$" . enh-ruby-mode))
+
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 
 ;;; Rspec
 (add-hook 'ruby-mode-hook 'rspec-mode)
@@ -27,68 +46,3 @@
 
 ;;; Do not compile SCSS on save, Rails does that for us
 (defvar scss-compile-at-save nil)
-
-;;; enhanced ruby mode
-(setq enh-ruby-program "/usr/local/opt/rbenv/versions/2.0.0-p195/bin/ruby")
-(autoload 'enh-ruby-mode "enh-ruby-mode" "Major mode for ruby files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rabl$" . enh-ruby-mode))
-
-(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
-
-;;; ruby-mode (obsolete)
-
-;; ;;; treat rabl files as ruby
-;; (setq auto-mode-alist (cons '("\\.rabl$" . ruby-mode) auto-mode-alist))
-
-;; ;;; Add ido-imenu support back into ruby-mode in Emacs24.
-;; ;;; See http://stackoverflow.com/q/12703110/
-;; (add-hook 'ruby-mode-hook
-;;           (lambda ()
-;;             (set (make-local-variable imenu-generic-expression)
-;;                  '(("Methods"  "^\\( *\\(def\\) +.+\\)"          1)
-;;                    ))))
-
-;; ;;; flymake ruby code:
-;; ;;; http://blog.senny.ch/blog/2012/10/06/emacs-tidbits-for-ruby-developers/
-;; (eval-after-load 'ruby-mode
-;;   '(progn
-;;      ;; Libraries
-;;      (require 'flymake)
-
-;;      ;; Invoke ruby with '-c' to get syntax checking
-;;      (defun flymake-ruby-init ()
-;;        (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                           'flymake-create-temp-inplace))
-;;               (local-file (file-relative-name
-;;                            temp-file
-;;                            (file-name-directory buffer-file-name))))
-;;          (list "ruby" (list "-c" local-file))))
-
-;;      (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-;;      (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-
-;;      (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-;;            flymake-err-line-patterns)
-
-;;      (add-hook 'ruby-mode-hook 'cabbage-flymake-init)
-
-;;      (add-hook 'ruby-mode-hook
-;;                (lambda ()
-;;                  (when (and buffer-file-name
-;;                             (file-writable-p
-;;                              (file-name-directory buffer-file-name))
-;;                             (file-writable-p buffer-file-name)
-;;                             (if (fboundp 'tramp-list-remote-buffers)
-;;                                 (not (subsetp
-;;                                       (list (current-buffer))
-;;                                       (tramp-list-remote-buffers)))
-;;                               t))
-;;                    (local-set-key (kbd "C-c d")
-;;                                   'flymake-display-err-menu-for-current-line)
-;;                    (flymake-mode t))))))
