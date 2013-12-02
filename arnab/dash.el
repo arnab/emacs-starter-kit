97d8026352a0658b1c97c1b49416663c9152dd9a
Arnab Deka <arnab.deka@gmail.com>
2013-12-02 15:14:58 +0530

    Ruby mode for dash.

--- arnab/dash.el
+++ arnab/dash.el
@@ -1,1 +1,4 @@
 (global-set-key "\C-cd" 'dash-at-point)
+(add-to-list 'dash-at-point-mode-alist '(enh-ruby-mode . "rb"))
+(add-hook 'rinari-minor-mode-hook
+          (lambda () (setq dash-at-point-docset "rails")))
