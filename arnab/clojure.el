e3e892f6aa830653627605bda8f2341260c0ff49
Arnab Deka <arnab.deka@gmail.com>
2014-07-31 15:49:19 +0530

    non conflicting keybinding for clj-refactor.

--- arnab/clojure.el
+++ arnab/clojure.el
@@ -1,4 +1,3 @@
 (add-hook 'clojure-mode-hook (lambda ()
                                (clj-refactor-mode 1)
-                               (cljr-add-keybindings-with-prefix "C-c C-m")
-                               ))
+                               (cljr-add-keybindings-with-prefix "C-x C-r")))
