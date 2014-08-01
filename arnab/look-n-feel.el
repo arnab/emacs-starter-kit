812d768704772b022419955861fdf4abe1cfec83
Arnab Deka <arnab.deka@gmail.com>
2014-08-01 22:31:36 +0530

    larger font on laptop.

--- arnab/look-n-feel.el
+++ arnab/look-n-feel.el
@@ -19,11 +19,11 @@
                                                 "Droid Sans Mono-18"
                                                 "Consolas-18"))
           (set-face-attribute 'default nil :font
-                              (font-candidate '"Fira Mono-13"
-                                              "Source Code Pro-13"
-                                              "DejaVu Sans Mono-13"
-                                              "Droid Sans Mono-13"
-                                              "Consolas-13"))))))
+                              (font-candidate '"Fira Mono-15"
+                                              "Source Code Pro-15"
+                                              "DejaVu Sans Mono-15"
+                                              "Droid Sans Mono-15"
+                                              "Consolas-15"))))))
 ;; Fontify current frame
 (fontify-frame nil)
 ;; Fontify any future frames
