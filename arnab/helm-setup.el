3b8bc8a073d25f5eebb2263ecaccbdaf8d75774c
Deka <arnadeka@0c4de9c3c2d9.ant.amazon.com>
2014-11-05 16:01:52 -0800

    don't use helm or ido anymore.

--- arnab/helm-setup.el
+++ arnab/helm-setup.el
@@ -1,15 +1,15 @@
-(require 'helm-config)
-(require 'helm-ls-git)
+;; (require 'helm-config)
+;; (require 'helm-ls-git)
 
-;;;###autoload
-(defun helm-mini ()
-  "My additions (all git file) to Preconfigured `helm' lightweight version \(buffer -> recentf\)."
-  (interactive)
-  (require 'helm-files)
-  (helm-other-buffer '(
-    helm-c-source-buffers-list
-    helm-c-source-ls-git-status
-    helm-c-source-recentf
-    helm-c-source-ls-git
-    helm-c-source-buffer-not-found)
-  "*helm mini*"))
+;; ;;;###autoload
+;; (defun helm-mini ()
+;;   "My additions (all git file) to Preconfigured `helm' lightweight version \(buffer -> recentf\)."
+;;   (interactive)
+;;   (require 'helm-files)
+;;   (helm-other-buffer '(
+;;     helm-c-source-buffers-list
+;;     helm-c-source-ls-git-status
+;;     helm-c-source-recentf
+;;     helm-c-source-ls-git
+;;     helm-c-source-buffer-not-found)
+;;   "*helm mini*"))
